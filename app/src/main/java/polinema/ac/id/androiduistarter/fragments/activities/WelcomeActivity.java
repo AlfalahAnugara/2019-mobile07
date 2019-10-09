1136b6e2c3c602221f69d8ff451e06b987e68c4c
AlfalahAnugara <Alfalah190299@gmail.com>
2019-10-09 23:22:18 +0700

    create Splash and Welcome activity

--- app/src/main/java/polinema/ac/id/androiduistarter/fragments/activities/WelcomeActivity.java
+++ app/src/main/java/polinema/ac/id/androiduistarter/fragments/activities/WelcomeActivity.java
@@ -1,16 +1,14 @@
-package polinema.ac.id.androiduistarter.activities;
+package polinema.ac.id.androiduistarter.fragments.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import polinema.ac.id.androiduistarter.R;
-
-public class MainActivity extends AppCompatActivity {
+public class WelcomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_welcome);
     }
 }
