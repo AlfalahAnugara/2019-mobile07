d07c1b3ac9952843c8b9207f98efdf61f2dfa7db
AlfalahAnugara <Alfalah190299@gmail.com>
2019-10-09 23:25:38 +0700

    Splash Activity

--- app/src/main/java/polinema/ac/id/androiduistarter/fragments/activities/WelcomeActivity.java
+++ app/src/main/java/polinema/ac/id/androiduistarter/fragments/activities/WelcomeActivity.java
@@ -1,10 +1,22 @@
 package polinema.ac.id.androiduistarter.fragments.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import polinema.ac.id.androiduistarter.R;
 
 public class WelcomeActivity extends AppCompatActivity {
+    private ViewPager viewPager;
+    private MyViewPagerAdapter myViewPagerAdapter;
+    private LinearLayout dotsLayout;
+    private TextView[] dots;
+    private int[] layouts;
+    private Button btnSkip, btnNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
